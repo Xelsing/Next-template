1b070e713d79b8211dd0b4e1693385d87eb6842b
Ivan Yarigin <vano.03.06@gmail.com>
2025-08-01 19:56:51 +0300

    feat(project): add stylelint scss

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,10 @@ const buildEslintCommand = (filenames) =>
   `eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --fix ')}`;
 const prettierCommand = (filenames) =>
   `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
+const stylelintCommand = (filenames) =>
+  `stylelint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, prettierCommand],
+  '*.{js,jsx}': [buildEslintCommand, prettierCommand],
+  '*.{css,scss}': [stylelintCommand],
 };
